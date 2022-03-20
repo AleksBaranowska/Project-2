23fb36652ab375948530e4f86521e66b2fd07a0f
AleksBaranowska <aleksandra.baranowska@protonmail.com>
2022-03-20 15:43:07 +0000

    Add functions responsible for game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,20 +14,83 @@ var playerScore = 0;
 var playerName = null;
 var rounds = 10;
 var scores = new Array();
-if(localStorage['scores']){
+if (localStorage['scores']) {
     scores = JSON.parse(localStorage.getItem("scores"));
 }
 
-function init(){
+function init() {
     playerName = document.getElementById('name').value;
-    if (playerName==''){
-        document.getElementById('error').parentNode.style.display='block';
-        document.getElementById('error').innerHTML="Please Enter a Valid Name!";
+    if (playerName == '') {
+        document.getElementById('error').parentNode.style.display = 'block';
+        document.getElementById('error').innerHTML = "Please Enter a Valid Name!";
+    } else {
+        landingScreen.style.display = 'none';
+        document.getElementById('playerName').innerHTML = playerName + " : ";
+        main.style.display = 'block';
     }
-    else{
-        landingScreen.style.display='none';
-        document.getElementById('playerName').innerHTML=playerName+" : ";
-        main.style.display='block';
+
+}
+
+function play() {
+    let userInput = document.getElementsByClassName('option');
+    for (let i = 0; i < userInput.length; i++) {
+        if (userInput[i].style.borderColor == 'red') {
+            playerChoice = userInput[i].id;
+            computeResults();
+            return;
+        }
     }
-    
+}
+
+var selectedOption = function () {
+    resetRound();
+    this.style.borderColor = 'red';
+    this.style.boxShadow = '0 0 10px rgb(255, 0, 0)';
+    play();
+};
+
+for (var i = 0; i < options.length; i++) {
+    options[i].addEventListener('click', selectedOption, false);
+}
+
+function computeResults() {
+    getComputerChoice();
+    resultBox.parentNode.style.display = 'block';
+    if (computerChoice === playerChoice) {
+        resultBox.innerHTML = "Game Drawn!";
+    } else if (playerChoice === "rock") {
+        if (computerChoice === "paper") {
+            resultBox.innerHTML = "Computer \"" + computerChoice + "\" Wins!";
+            computerScoreBox.innerHTML = ++computerScore;
+        } else {
+            resultBox.innerHTML = "Player \"" + playerChoice + "\" Wins!";
+            playerScoreBox.innerHTML = ++playerScore;
+        }
+    } else if (playerChoice === "paper") {
+        if (computerChoice === "scissors") {
+            resultBox.innerHTML = "Computer \"" + computerChoice + "\" Wins!";
+            computerScoreBox.innerHTML = ++computerScore;
+        } else {
+            resultBox.innerHTML = "Player \"" + playerChoice + "\" Wins!";
+            playerScoreBox.innerHTML = ++playerScore;
+        }
+    } else if (playerChoice === "scissors") {
+        if (computerChoice === "rock") {
+            resultBox.innerHTML = "Computer \"" + computerChoice + "\" Wins!";
+            computerScoreBox.innerHTML = ++computerScore;
+        } else {
+            resultBox.innerHTML = "Player \"" + playerChoice + "\" Wins!";
+            playerScoreBox.innerHTML = ++playerScore;
+        }
+    }
+    --rounds;
+
+    if (rounds > 0) {
+        document.getElementById('round').innerHTML = rounds;
+    } else {
+        document.getElementById('round').innerHTML = rounds;
+        scores.push(new Array(playerName, playerScore, computerScore));
+        localStorage.setItem("scores", JSON.stringify(scores));
+        showScores();
+    }
 }
