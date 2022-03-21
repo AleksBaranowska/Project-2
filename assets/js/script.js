038f445c746451833ef17a25ac6673df763112bb
AleksBaranowska <aleksandra.baranowska@protonmail.com>
2022-03-21 07:11:44 +0000

    Fix filepaths

--- assets/js/script.js
+++ assets/js/script.js
@@ -138,9 +138,9 @@ function showLeaderBoard() {
 }
 
 function descendingSort(a, b) {
-    if (a[1] > b[1]) return -1
-    if (a[1] < b[1]) return 1
-    return 0
+    if (a[1] > b[1]) return -1;
+    if (a[1] < b[1]) return 1;
+    return 0;
 }
 
 function getComputerChoice() {
